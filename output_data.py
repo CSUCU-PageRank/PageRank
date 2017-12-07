e99ce36f61e11796af217fe432517707c7b6f895
victoria-yuzkiv <yuzkiv@ucu.edu.ua>
2017-12-07 19:32:45 +0200

    add chart

--- output_data.py
+++ output_data.py
@@ -1,7 +1,27 @@
 from pagerank import *
+import matplotlib.pyplot as plt
 
 
-def output_data():
-    data = page_rank('data.txt')
+def output_data(info):
+    data = page_rank(info)
     res = power_iteration(data[0], data[1])
     return create_top_ranks(res)
+
+
+def create_chart(info):
+    lst = output_data(info)
+    plt.rcdefaults()
+    objects = (lst[i][1] for i in range(len(lst)))
+    y_pos = np.arange(10)
+    performance = [lst[i][0] for i in range(len(lst))]
+    plt.bar(y_pos, performance, color='r', align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Rank')
+    plt.xlabel('Page number')
+    plt.title('Top 10 pages')
+    plt.show()
+
+
+data = 'data.txt'
+print(output_data(data))
+create_chart(data)
